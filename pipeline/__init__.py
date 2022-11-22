bbfd2bbb4b0328da30a36b903df310de25c2e106
vishnuwe45 <87526447+vishnuwe45@users.noreply.github.com>
2022-11-22 12:08:43 +0530

    Update __init__.py

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,6 +1,5 @@
 from gaiasdk import sdk
 import logging
-import time
 import subprocess
 import os
 import git
@@ -13,42 +12,14 @@ from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.proxy import *
 import sys
-from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-proxy_host = os.environ.get('ZAP_IP','localhost')
-proxy_port = os.environ.get('ZAP_PORT',8090)
-proxy_url = "http://{0}:{1}".format(proxy_host,proxy_port)
-target_site = os.environ.get('TARGET_URL','http://134.209.146.136')
-zap = ZAP(proxies={
-            "http": "http://{0}:{1}".format(proxy_host, proxy_port), 
-            "https": "http://{0}:{1}".format(proxy_host, proxy_port)}
-            )
-context_id_list = []
-
 def StartZAP(args):
             logging.info("ZAP Initiated")
             time.sleep(5)
-            cmd = "/app/ZAP_2.7.0/zap.sh -daemon -host localhost -port 8090 -config api.disablekey=true -config 'api.addrs.addr.name=.*' -config api.addrs.addr.regex=true"
-            process = subprocess.Popen(cmd.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE,universal_newlines=True)
-            stdout, stderr = process.communicate()
-            logging.info(stdout)
-            logging.info(stderr)
-            time.sleep(20)
-            logging.info("ZAP Started")
             
-            regex = "{0}.*".format(target_site)
-            context_id = zap.context.new_context(contextname="CTF2")
-            time.sleep(1)
-            logging.info("Context Started")
-            zap.context.include_in_context(contextname, regex=regex)
-            time.sleep(5)
-            logging.info(context_id)
-            context_id_list.append(context_id)
-            logging.info(context_id_list)
-            logging.info("CTF2 Context set successfully")
             logging.info("==================================================")
     
 def RunScript(args):
